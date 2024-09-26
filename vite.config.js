08e1b35c549f4e8f8193877979408cff96049ca9
SukiMass <sureshkrishna.avi@hotmail.com>
2024-09-26 11:29:55 +0530

    vite deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "/adra_mern_questions",
   server: {
 
     host: '10.10.24.16' // Listen on all network interfaces [3, 7]
